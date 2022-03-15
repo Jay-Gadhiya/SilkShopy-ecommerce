6bf0fdf02cf487ce4501986e443699b430dfa54a
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-03-15 11:54:23 +0530

    feat : Add navbar in app.js

--- silkshopy-ecommerce/src/App.js
+++ silkshopy-ecommerce/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import logo from "./logo.png";
 import {products} from "./backend/db/products"
+import { Navbar } from "./components/Navbar/Navbar";
 function App() {
   return (
-   <>
-    
-   </>
+   <Navbar />
   )
 }
 
