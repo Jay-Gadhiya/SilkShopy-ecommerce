25715c2b4946c34d0754f5a8eaf95117e7ec9937
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-03-15 12:25:57 +0530

    feat : made home page

--- silkshopy-ecommerce/src/App.js
+++ silkshopy-ecommerce/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.png";
 import {products} from "./backend/db/products"
 import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
+import { Home } from "./pages/Homepage/Home";
 
 function App() {
   return (
