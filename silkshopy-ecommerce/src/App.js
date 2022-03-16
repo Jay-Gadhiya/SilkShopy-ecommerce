5c642f2b61e6980afa0bc07c6ad50265536aa4f7
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-03-16 17:16:09 +0530

    refactor : indentation

--- silkshopy-ecommerce/src/App.js
+++ silkshopy-ecommerce/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import logo from "./logo.png";
-import {products} from "./backend/db/products"
+import { products } from "./backend/db/products"
 import { Home } from "./pages/Homepage/Home";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Routes, Route, Link } from "react-router-dom";
-import {ProductListing} from "../src/pages/ProductListing/ProductListing";
-import {Cart} from "../src/pages/Cart/Cart";
-import {Wishlist} from "../src/pages/Wishlist/Wishlist";
-import {Login} from "../src/pages/Authentication/Login";
-import {Signup} from "../src/pages/Authentication/Signup";
+import { ProductListing } from "../src/pages/ProductListing/ProductListing";
+import { Cart } from "../src/pages/Cart/Cart";
+import { Wishlist } from "../src/pages/Wishlist/Wishlist";
+import { Login } from "../src/pages/Authentication/Login";
+import { Signup } from "../src/pages/Authentication/Signup";
 
 
 function App() {
