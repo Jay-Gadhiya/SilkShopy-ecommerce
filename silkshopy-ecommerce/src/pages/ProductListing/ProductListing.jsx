23554e149938ebffa88a22aa1cc9eec4ebf61f34
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-03-19 15:03:17 +0530

    feat : make export function

--- silkshopy-ecommerce/src/pages/ProductListing/ProductListing.jsx
+++ silkshopy-ecommerce/src/pages/ProductListing/ProductListing.jsx
@@ -2,55 +2,38 @@ import { Card } from "../../components/Card";
 import { Footer } from "../../components/Footer/Footer";
 import { Filters } from "./components/Filter";
 import "./ProductListing.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { useFilter } from "../../contexts/context/filter-context";
-import { sortByPrice } from "./functions/sort-price";
-import { sortByRating } from "./functions/sort-rating";
-import { priceRangeFilter } from "./functions/price-range";
-import { catagoryFilter } from "./functions/catagory-filter";
+import { catagoryFilter, sortByRating, sortByPrice, priceRangeFilter, useFetchData } from "./functions/export";
 
 const ProductListing = () => {
-  const [product, setProduct] = useState([]);
 
+  const  { product }  = useFetchData();
   const { state } = useFilter();
   const { sortBy, rating, showAllProducts, laptopOnly, phoneOnly, headPhoneOnly, gamingOnly, priceRange } = state;
 
+  // filter functions
   const catagoryData = catagoryFilter(product, laptopOnly, phoneOnly, headPhoneOnly, gamingOnly);
   const ratingData = sortByRating(catagoryData, rating);
   const sortedData = sortByPrice(sortBy, ratingData);
   const priceRangeData = priceRangeFilter(sortedData, priceRange);
 
 
-  useEffect(
-    () =>
-      (async () => {
-        try {
-          const res = await axios.get("/api/products");
-          setProduct(() => res.data.products);
-        } catch (error) {
-          console.log(error);
-        }
-      })(),
-    []
-  );
-
   return (
     <>
       <div className="page-container">
+
         <Filters />
 
         <main className="product-listing-container">
           <h2 className="product-main-heading margin-bottom">Products</h2>
 
           <div className="product-cards">
-            {product &&
-              priceRangeData.map((item) => (
-                <Card key={item.id} productData = {item} />
-              ))}
+            {product && priceRangeData.map((item) => ( <Card key={item.id} productData = {item} /> ))}
           </div>
         </main>
+        
       </div>
+
       <Footer />
     </>
   );
