d83c1c3fc46f85776bf9644f45960dea190abb3e
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-03-19 18:20:14 +0530

    refactor : make unmuted array value

--- silkshopy-ecommerce/src/pages/ProductListing/functions/sort-price.jsx
+++ silkshopy-ecommerce/src/pages/ProductListing/functions/sort-price.jsx
@@ -1,10 +1,10 @@
 const sortByPrice = (sortBy, priceRangeData) => {
 
     if (sortBy === "LOW_TO_HIGH")
-        return priceRangeData.sort((a, b) => Number(a.price) - Number(b.price));
+        return [...priceRangeData].sort((a, b) => Number(a.price) - Number(b.price));
 
     if (sortBy === "HIGH_TO_LOW")
-        return priceRangeData.sort((a, b) => Number(b.price) - Number(a.price));
+        return [...priceRangeData].sort((a, b) => Number(b.price) - Number(a.price));
 
     return priceRangeData;
 }
