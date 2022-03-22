cdd9a21b1c147f91cec93007d0189b129b8e67f8
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-03-22 19:53:28 +0530

    style : add syle in cart

--- silkshopy-ecommerce/src/pages/Cart/Cart.jsx
+++ silkshopy-ecommerce/src/pages/Cart/Cart.jsx
@@ -3,6 +3,8 @@ import { useCart } from "../../contexts/context/cart-context";
 import "./Cart.css";
 import { CartBill } from "./components/CartBill"
 import { CartProduct } from "./components/CartProduct"
+import { emptyCart } from "../../assets/export";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
 
@@ -12,18 +14,34 @@ const Cart = () => {
         <>
             <h2 className="my-cart-heading">My Cart</h2>
 
-            <main className="purchase-area flex">
+           {
+           cartState.cart.length !== 0 
+
+           ?
+           <> 
+           <main className="purchase-area flex">
                 <div className="column-cards">
 
                     {cartState.cart && cartState.cart.map(item => (
                          <CartProduct key={ item.id } product = { item } />
                     )) }
-                    
+
                 </div>
                 
                 <CartBill />
-            </main>
+            </main> 
             <Footer />
+            </>
+
+            :
+            <div className="empty-cart" >
+                <img className="empty-cart-img" src= {emptyCart} alt="image" />
+                <p className=" subtext-cart pd-left">Your cart is empty!</p>
+                <button class="btn btn-primary cart-btn pd-left"> <Link to="/productListing">Shop Now</Link></button>
+            </div>
+
+            }
+            
         </>
 
     )
